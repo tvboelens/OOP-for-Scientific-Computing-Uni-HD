93a4567046576c67b4368c8b0b1931f36701c60f
tvboelens <tvboelens@gmail.com>
2024-07-05 14:42:26 +0200

    Constructor for Rational class

--- Sheet-2/rational.cpp
+++ Sheet-2/rational.cpp
@@ -23,13 +23,37 @@ int gcd(int x, int y)
     return largest;
 }
 
-
+class Rational
+{
+    private:
+        int m_denominator {};
+        int m_numerator {};
+    public:
+        Rational()
+            : m_denominator { 1 }, m_numerator { 1 }
+            {
+                std::cout << "Rational " << m_denominator
+                          << "/" << m_numerator << "constructed \n";
+            }
+        Rational(int denominator, int numerator)
+            : m_denominator { denominator/gcd(denominator, numerator) }, m_numerator { numerator/gcd(denominator, numerator) }
+            {
+                std::cout << "Rational " << m_denominator
+                          << "/" << m_numerator << "constructed \n";
+            }
+        Rational(int integer)
+            : m_denominator { integer }, m_numerator { 1 }
+            {
+                std::cout << "Rational " << m_denominator
+                          << "/" << m_numerator << "constructed \n";
+            }
+};
 
 int main()
 {
     int a, b;
     std::cout << "Eingabe a,b für gcd(a,b)";
     std::cin >> a >> b;
-    std::cout << "gcd(a,b) = " << gcd(a, b) << std::endl;
+    Rational q{ a, b };
     return 0;
 }
