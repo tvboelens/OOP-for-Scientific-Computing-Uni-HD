92fe005b0be72594b0e1504ad125c9f344b9d010
tvboelens <tvboelens@gmail.com>
2024-07-06 16:26:15 +0200

    Added binary operators

--- Sheet-2/rational.cpp
+++ Sheet-2/rational.cpp
@@ -4,15 +4,15 @@ int gcd(int x, int y)
 {
     int largest, smallest, remainder;
    
-    if (x > y)
+    if (std::abs(x) > std::abs(y))
     {
-        largest = x;
-        smallest = y;
+        largest = std::abs(x);
+        smallest = std::abs(y);
     }
     else 
     {
-        largest = y;
-        smallest = x;
+        largest = std::abs(y);
+        smallest = std::abs(x);
     }
     while (smallest>0)
     {
@@ -26,34 +26,97 @@ int gcd(int x, int y)
 class Rational
 {
     private:
+        int m_numerator{};
         int m_denominator {};
-        int m_numerator {};
     public:
         Rational()
-            : m_denominator { 1 }, m_numerator { 1 }
+            : m_numerator{1} , m_denominator{1}
+        {
+            /* std::cout << "Rational " << m_numerator << 
+            "/" << m_denominator << " constructed \n"; */
+        };
+        Rational(int numerator, int denominator)
+            : m_denominator{denominator / gcd(denominator, numerator)},
+              m_numerator{numerator / gcd(denominator, numerator)}
+        {
+            if  (m_denominator<0)
             {
-                std::cout << "Rational " << m_denominator
-                          << "/" << m_numerator << "constructed \n";
+                m_denominator *= -1;
+                m_numerator += -1;
             }
-        Rational(int denominator, int numerator)
-            : m_denominator { denominator/gcd(denominator, numerator) }, m_numerator { numerator/gcd(denominator, numerator) }
-            {
-                std::cout << "Rational " << m_denominator
-                          << "/" << m_numerator << "constructed \n";
-            }
+            //std::cout << "Rational " << m_numerator << "/" << m_denominator << " constructed \n";
+        };
         Rational(int integer)
-            : m_denominator { integer }, m_numerator { 1 }
-            {
-                std::cout << "Rational " << m_denominator
-                          << "/" << m_numerator << "constructed \n";
-            }
+            : m_numerator{integer}, m_denominator{1}
+        {
+            /* std::cout << "Rational " << m_numerator
+                      << "/" << m_denominator << " constructed \n"; */
+        };
+        int get_denominator() const { return m_denominator; }
+        int get_numerator() const { return m_numerator; }
 };
+
+Rational operator+(const Rational& p, const Rational& q)
+{
+    return Rational{
+        p.get_denominator()*q.get_numerator() + p.get_numerator()*q.get_denominator(), 
+        p.get_denominator()*q.get_denominator()};
+}
+
+Rational operator+(const Rational& p, const int& k)
+{
+    return p + Rational{k, 1};
+}
+
+Rational operator-(const Rational& p)
+{
+    return Rational{-p.get_numerator(), p.get_numerator()};
+}
+
+Rational operator-(const Rational& p, const Rational& q)
+{
+    return p + -q;
+}
+
+Rational operator-(const Rational& p, const int& k)
+{
+    return p - Rational{k};
+}
+
+Rational operator*(const Rational& p, const Rational& q)
+{
+    return Rational{p.get_numerator() * q.get_numerator(),
+                    p.get_denominator() * q.get_denominator()};
+}
+
+Rational operator*(const Rational& p, const int& k)
+{
+    return p*Rational{k};
+}
+
+Rational operator/(const Rational& p, const Rational& q)
+{
+    return Rational{p.get_numerator() * q.get_denominator(),
+                    p.get_denominator() * q.get_numerator()};
+}
+
+Rational operator/(const Rational& p, const int& k)
+{
+    return p / Rational{1, k};
+}
 
 int main()
 {
-    int a, b;
-    std::cout << "Eingabe a,b für gcd(a,b)";
-    std::cin >> a >> b;
-    Rational q{ a, b };
+    Rational f1{ -3, 12 };
+    Rational f2{4, 3};
+
+    std::cout << "f1+f2 = " << (f1 + f2).get_numerator() <<"/" << (f1+f2).get_denominator() << "\n";
+    std::cout << "f1*f2 = " << (f1*f2).get_numerator() << "/" << (f1*f2).get_denominator() << "\n";
+    std::cout << "4+f2 = " << (4 + f2).get_numerator() << "/" << (4 + f2).get_denominator() << "\n";
+    std::cout << "f2+5 = " << (f2+5).get_numerator() << "/" << (f2+5).get_denominator() << "\n";
+    std::cout << "12*f1 = " << (12*f1).get_numerator() << "/" << (12*f1).get_denominator() << "\n";
+    std::cout << "f1*6 = " << (f1*6).get_numerator() << "/" << (f1*6).get_denominator() << "\n";
+    std::cout << "f1/f2 = " << (f1 / f2).get_numerator() << "/" << (f1 / f2).get_denominator() << "\n";
+
     return 0;
 }
