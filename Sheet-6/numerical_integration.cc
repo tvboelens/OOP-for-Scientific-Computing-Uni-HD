0a919351d7ec4f83bf033e656ac4d358a2f21d7e
tvboelens <tvboelens@gmail.com>
2024-07-26 21:26:13 +0200

    Missing constructor and mistake in fit

--- Sheet-6/numerical_integration.cc
+++ Sheet-6/numerical_integration.cc
@@ -1,39 +1,44 @@
 #include "numerical_integration.hh"
 #include <vector>
+#include <iostream>
 
 class Trapezoidal: public Quadrature
 {
-    virtual double fit(double left_endpoint, double right_endpoint) const override
-    {
-        double Q = {m_func->operator()(left_endpoint) + m_func->operator()(right_endpoint)};
-        Q *= (right_endpoint - left_endpoint)/2;
-        return Q;
-    }
+    public:
+        virtual double fit(double left_endpoint, double right_endpoint) const override
+        {
+            double Q = {m_func->operator()(left_endpoint) + m_func->operator()(right_endpoint)};
+            Q *= (right_endpoint - left_endpoint)/2;
+            return Q;
+        }
+        Trapezoidal(const Function &func) : Quadrature{func} {};
 };
 
 class Simpson : public Quadrature
 {
-    virtual double fit(double left_endpoint, double right_endpoint) const override
-    {
-        double midpoint{(left_endpoint + right_endpoint) / 2};
-        double Q{m_func->operator()(left_endpoint) + 4*m_func->operator()(midpoint) + m_func->operator()(right_endpoint)};
-        Q *= (right_endpoint - left_endpoint) / 6;
-        return Q;
-    }
+    public:
+        virtual double fit(double left_endpoint, double right_endpoint) const override
+        {
+            double midpoint{(left_endpoint + right_endpoint) / 2};
+            double Q{m_func->operator()(left_endpoint) + 4*m_func->operator()(midpoint) + m_func->operator()(right_endpoint)};
+            Q *= (right_endpoint - left_endpoint) / 6;
+            return Q;
+        }
+        Simpson(const Function &func) : Quadrature{func} {};
 };
 
 void Integral::fit(int num_subintervals)
 {
     if (num_subintervals!=0)
     {
-        double integral{0};
         subintegrals.resize(num_subintervals);
         for (size_t i = 0; i < subintegrals.size();++i)
         {
             subintegrals.at(i) = m_quad->fit(
-                m_left_endpoint + i / num_subintervals,
-                m_left_endpoint + (i + 1) / num_subintervals);
-            integral += subintegrals[i];
+                m_left_endpoint + 
+                    (static_cast<double>(i) / num_subintervals) * (m_right_endpoint - m_left_endpoint),
+                m_left_endpoint + 
+                    ((static_cast<double>(i) + 1.) / num_subintervals) * (m_right_endpoint - m_left_endpoint));
         }
     }
 }
