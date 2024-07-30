e2cff9b18ec23eb15c4265e321ab9fdbfb08b090
tvboelens <tvboelens@gmail.com>
2024-07-30 16:31:35 +0200

    Testing for sine and adaptive quadrature

--- Sheet-6/testintegration.cc
+++ Sheet-6/testintegration.cc
@@ -1,7 +1,9 @@
 #include "numerical_integration.cc"
 #include <iostream>
+#include <numbers>
 #include <vector>
 #include <cmath>
+
 
 class Polynomial: public Function
 {
@@ -21,6 +23,28 @@ class Polynomial: public Function
         std::vector<double> m_coefficients;
 };
 
+class Sine: public Function
+{
+    public:
+        double operator()(double x) const override
+        {
+            double output{std::sin(x)};
+            output *= (x * std::numbers::inv_pi);
+            return output;
+        }
+};
+
+class SineIntegrated : public Function
+{
+public:
+    double operator()(double x) const override
+    {
+        double output{std::sin(x) - x * std::cos(x)};
+        output *= std::numbers::inv_pi;
+        return output;
+    }
+};
+
 int main()
 {
     std::vector<double> coefficients{5, 0, 2};
@@ -36,14 +60,55 @@ int main()
     Trapezoidal QT{p};
     Integral IS{p, QS, -3.0, 13.0};
     Integral IT{p, QT, -3.0, 13.0};
-    for (int i = 1; i <=15;++i)
-        {
-            IS.fit(i); // Something is wrong, for num_subintervals = 1 the answer is correct, but not for > 1
-            IT.fit(i);
+    for (int i = 1; i <=20;++i)
+    {
+            IS.setNumSubintervals(i); // Something is wrong, for num_subintervals = 1 the answer is correct, but not for > 1
+            IS.fit();
+            IT.setNumSubintervals(i);
+            IT.fit();
             std::cout << "num_subintervals = " << i << "\n";
             std::cout << "True integral = " << q(13) - q(-3) << "\n";
             std::cout << "Integral according to Simpson method = " << IS.evaluate() << "\n";
             std::cout << "Integral according to trapezoidal method = " << IT.evaluate() << "\n";
     }
+
+    IS.setNumSubintervals(0); // Something is wrong, for num_subintervals = 1 the answer is correct, but not for > 1
+    IS.fit();
+    IT.setNumSubintervals(0);
+    IT.fit();
+    std::cout << "Using adptive integration...\n";
+    std::cout << "True integral = " << q(13) - q(-3) << "\n";
+    std::cout << "Integral according to Simpson method = " << IS.evaluate() << "\n";
+    std::cout << "Integral according to trapezoidal method = " << IT.evaluate() << "\n";
+
+    Sine f;
+    SineIntegrated F;
+    double n{20};
+    
+    Simpson QS_sine{f};
+    Trapezoidal QT_sine{f};
+    Integral IS_sine{f, QS_sine, 0.0, 2 * std::numbers::pi};
+    Integral IT_sine{f, QT_sine, 0.0, 2 * std::numbers::pi};
+    for (int i = 1; i <= 20; ++i)
+    {
+        IS_sine.setNumSubintervals(i); // Something is wrong, for num_subintervals = 1 the answer is correct, but not for > 1
+        IS_sine.fit();
+        IT_sine.setNumSubintervals(i);
+        IT_sine.fit();
+        std::cout << "num_subintervals = " << i << "\n";
+        std::cout << "True integral = " << F(2*std::numbers::pi) - F(0) << "\n";
+        std::cout << "Integral according to Simpson method = " << IS_sine.evaluate() << "\n";
+        std::cout << "Integral according to trapezoidal method = " << IT_sine.evaluate() << "\n";
+    }
+
+    IS_sine.setNumSubintervals(0); // Something is wrong, for num_subintervals = 1 the answer is correct, but not for > 1
+    IS_sine.fit(1e-5);
+    IT_sine.setNumSubintervals(0);
+    IT_sine.fit(1e-5);
+    std::cout << "Using adptive integration...\n";
+    std::cout << "True integral = " << F(2 * std::numbers::pi) - F(0) << "\n";
+    std::cout << "Integral according to Simpson method = " << IS_sine.evaluate() << "\n";
+    std::cout << "Integral according to trapezoidal method = " << IT_sine.evaluate() << "\n";
+    return 0;
     return 0;
 }
